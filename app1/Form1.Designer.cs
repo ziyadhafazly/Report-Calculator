f23bef383886d50ba5a757f83331186d3ff4e5bd
ziyadhafazly <fasly2570@gmail.com>
2024-11-03 19:20:04 +0530

    Add project files.

---
+++ app1/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace app1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textNAME = new System.Windows.Forms.TextBox();
+            this.textICT = new System.Windows.Forms.TextBox();
+            this.textSFT = new System.Windows.Forms.TextBox();
+            this.textET = new System.Windows.Forms.TextBox();
+            this.textAVG = new System.Windows.Forms.TextBox();
+            this.textGRADE = new System.Windows.Forms.TextBox();
+            this.textTOTAL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(229, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Students Marks Calculator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(76, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ICT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(76, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 22);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "SFT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(76, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 22);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "E-TECH";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(76, 313);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 22);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(76, 358);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 22);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Average";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(76, 404);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 22);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Grade";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textNAME
+            // 
+            this.textNAME.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textNAME.Location = new System.Drawing.Point(291, 132);
+            this.textNAME.Name = "textNAME";
+            this.textNAME.Size = new System.Drawing.Size(418, 22);
+            this.textNAME.TabIndex = 2;
+            this.textNAME.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textICT
+            // 
+            this.textICT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textICT.Location = new System.Drawing.Point(291, 180);
+            this.textICT.Name = "textICT";
+            this.textICT.Size = new System.Drawing.Size(418, 22);
+            this.textICT.TabIndex = 2;
+            // 
+            // textSFT
+            // 
+            this.textSFT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textSFT.Location = new System.Drawing.Point(291, 223);
+            this.textSFT.Name = "textSFT";
+            this.textSFT.Size = new System.Drawing.Size(418, 22);
+            this.textSFT.TabIndex = 2;
+            // 
+            // textET
+            // 
+            this.textET.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textET.Location = new System.Drawing.Point(291, 269);
+            this.textET.Name = "textET";
+            this.textET.Size = new System.Drawing.Size(418, 22);
+            this.textET.TabIndex = 2;
+            // 
+            // textAVG
+            // 
+            this.textAVG.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textAVG.Location = new System.Drawing.Point(291, 358);
+            this.textAVG.Name = "textAVG";
+            this.textAVG.Size = new System.Drawing.Size(418, 22);
+            this.textAVG.TabIndex = 2;
+            // 
+            // textGRADE
+            // 
+            this.textGRADE.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textGRADE.Location = new System.Drawing.Point(291, 404);
+            this.textGRADE.Name = "textGRADE";
+            this.textGRADE.Size = new System.Drawing.Size(418, 22);
+            this.textGRADE.TabIndex = 2;
+            // 
+            // textTOTAL
+            // 
+            this.textTOTAL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textTOTAL.Location = new System.Drawing.Point(291, 313);
+            this.textTOTAL.Name = "textTOTAL";
+            this.textTOTAL.Size = new System.Drawing.Size(418, 22);
+            this.textTOTAL.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(414, 460);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "CALCULATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(843, 531);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textGRADE);
+            this.Controls.Add(this.textTOTAL);
+            this.Controls.Add(this.textAVG);
+            this.Controls.Add(this.textET);
+            this.Controls.Add(this.textSFT);
+            this.Controls.Add(this.textICT);
+            this.Controls.Add(this.textNAME);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "StuReport";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textNAME;
+        private System.Windows.Forms.TextBox textICT;
+        private System.Windows.Forms.TextBox textSFT;
+        private System.Windows.Forms.TextBox textET;
+        private System.Windows.Forms.TextBox textAVG;
+        private System.Windows.Forms.TextBox textGRADE;
+        private System.Windows.Forms.TextBox textTOTAL;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ app1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ICT, SFT, ET, TOT, AVG;    // changed name
+            String grade;
+
+            ICT = int.Parse(textICT.Text);
+            SFT=int.Parse(textSFT.Text);
+            ET=int.Parse(textET.Text);
+
+            TOT = ICT + SFT + ET;
+            textTOTAL.Text = TOT.ToString();
+
+            AVG = TOT / 3;
+            textAVG.Text = AVG.ToString();
+
+            if (AVG >= 70)
+            {
+                grade = "A";
+            }
+            else if(AVG>=60){
+                grade = "B";
+            }
+            else if (AVG >= 50)
+            {
+                grade= "C";
+            }
+            else {
+                grade = "w";
+            }
+            textGRADE.Text = grade.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
